1fe7d6103c21df8b19a0c7bb3b3f0be735888937
Madaminov <muhammadanasxon6694@gmail.com>
2023-02-08 00:20:38 +0500

    add

--- js/script.js
+++ js/script.js
@@ -23,10 +23,3 @@ window.onscroll = () =>{
     }
 }
 
-window.onload = () =>{
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
-}
