05348ff75a17610dc95d09707eaafca49be12d2f
Madaminov <muhammadanasxon6694@gmail.com>
2023-02-12 17:50:16 +0500

    add

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');    
     loginForm.classList.remove('active');
 }
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
